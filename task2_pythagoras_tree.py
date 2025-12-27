4a945231aca65c66ecba141d4058e413be3c7c70
Inna Saienko <inna.saienko.nl@gmail.com>
2025-12-27 09:45:14 +0100

    added draw_tree body of func

--- task2_pythagoras_tree.py
+++ task2_pythagoras_tree.py
@@ -4,18 +4,32 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def draw_tree(x, y, length, depth, max_depth):
+def draw_tree(x, y, length, angle, depth, max_depth):
     """Draw Pythagoras tree with recursion depth and color gradient"""
 
     if depth == 0:
         return
 
+    # x, y -> beginning of branch
+    # converting length and angle into final point coordinates for a line.
+    x_end = x + length * np.cos(angle)
+    y_end = y + length * np.sin(angle)
+    color = (depth / max_depth, 0.3, 1 - depth / max_depth)
+
+    plt.plot([x, x_end], [y, y_end], color=color, linewidth=2)
+    new_length = length * 0.7
+    draw_tree(x_end, y_end, new_length, angle + np.pi / 6, depth - 1, max_depth)
+    draw_tree(x_end, y_end, new_length, angle - np.pi / 6, depth - 1, max_depth)
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("depth", type=int, help="Recursion depth")
+    parser.add_argument("depth", type=int, nargs="?", default=6, help="Recursion depth")
     args = parser.parse_args()
     plt.figure(figsize=(8, 8))
     draw_tree(0, 0, 1, np.pi / 2, args.depth, args.depth)
     plt.axis("equal")
     plt.axis("off")
-    plt.show()
+    plt.savefig("pythagoras_tree.png", bbox_inches='tight')
+    print("Tree saved as pythagoras_tree.png")
