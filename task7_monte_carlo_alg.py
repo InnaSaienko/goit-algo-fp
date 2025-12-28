58c3ab2028e2d07f4f063dfb35cfbb615534512d
Inna Saienko <inna.saienko.nl@gmail.com>
2025-12-28 14:43:38 +0100

    complete Monte Carlo algorithm

--- task7_monte_carlo_alg.py
+++ task7_monte_carlo_alg.py
@@ -1,9 +1,21 @@
-def monte_carlo_dice(num_rolls: int = 100_000) -> tuple[dict, dict]:
+import numpy as np
+
+
+def monte_carlo_dice(num_rolls: int = 100_000) -> dict[int, float]:
     """Simulate dice rolls and compute sum probabilities"""
 
-    counts = {}
-    probabilities = {}
-    return counts, probabilities
+    die_1 = np.random.randint(1, 7, size=num_rolls)
+    die_2 = np.random.randint(1, 7, size=num_rolls)
+    sums = die_1 + die_2
+
+    counts = {s: 0 for s in range(2, 13)}
+    for s in sums:
+        counts[s] += 1
+
+    probabilities = {k: (v / num_rolls) * 100 for k, v in counts.items()}
+    return probabilities
+
+
 
 
 def print_table(probabilities: dict) -> None:
